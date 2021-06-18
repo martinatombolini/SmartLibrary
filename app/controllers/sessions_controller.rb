dbec9e78c1e35e109e486353b9a7c14bf4366c2b
simonalai <simona_lai@icloud.com>
2021-06-18 12:55:07 +0200

    aggiunta messaggi errore login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,12 @@ class SessionsController < ApplicationController
         username = params[:user][:username]
         password = params[:user][:password]
         @current_user = Library.where(:username => username)
-        if @current_user.length==0 then 
+        if @current_user.length==0 or @current_user[0][:password]!= password then 
+            flash[:loginerror]=true
             redirect_to login_path
-        else
-            if Library.where(:password => password).length==0 then 
-                redirect_to login_path
-            else 
-                session[:user] = @current_user[0][:username]
+            else
+                session[:user]=@current_user[0][:name]
                 redirect_to root_path
             end
-        end
     end
 end
