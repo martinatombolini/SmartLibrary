01f806f29fb305873665713edb8400f09074ad10
martinatombolini <tombolini.1844737@studenti.uniroma1.it>
2021-06-20 11:18:40 +0200

    operazioni CRUD

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,13 +8,38 @@ class BooksController < ApplicationController
     end
 
     def new
+        @book = Book.new
+    end
+
+    def edit
+        @book = Book.find(params[:id])
     end
 
     def create
         @book = Book.new(book_params)
  
-        @book.save
-        redirect_to @book
+        if @book.save
+            redirect_to @book
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @book = Book.find(params[:id])
+       
+        if @book.update(book_params)
+          redirect_to @book
+        else
+          render 'edit'
+        end
+    end
+
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+       
+        redirect_to books_path
     end
 
     private
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+    validates :title, presence: true
+    validates :notes, presence: true
+    validates :author, presence: true
+    validates :isbn, presence: true, length: { is: 13 }, numericality: { only_integer: true }
+
 end
