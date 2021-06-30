bc43e121275781b603d1dd46baa93672e9f4a2d2
simonalai <simona_lai@icloud.com>
2021-06-30 10:38:47 +0200

    Bottone "ask reservation" ok

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,7 @@ class BooksController < ApplicationController
     def show
         @book = Book.find(params[:id])
         @user = User.all
+        @reservation = Reservation.all
     end
 
     def new
