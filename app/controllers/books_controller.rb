e8952811eeee477b556a080f4fbd92c04cbf7a20
martinatombolini <tombolini.1844737@studenti.uniroma1.it>
2021-06-30 16:39:33 +0200

    aggiustamenti grafica

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
 
-    before_action :authenticate_user!, except: [:index]
+    before_action :authenticate_user!, except: [:index,:search]
 
     def index
         @books = Book.all
